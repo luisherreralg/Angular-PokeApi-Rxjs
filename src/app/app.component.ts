de5590d972a7b121f5f1258d1c7e837a9129204f
luisherreralg <lherreralg@outlook.es>
2023-02-12 20:15:08 +0100

    lkawjdlkakldja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import { PokeApiService } from './services/poke-api.service';
+import { OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Pokemon } from './types/Pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pokemon-test';
+export class AppComponent implements OnInit, OnDestroy {
+  pokemons: Pokemon[];
+  fetching = false;
+
+  private activeSubscription: Subscription[] = [];
+
+  constructor(private apiService: PokeApiService) {
+    this.pokemons = [];
+  }
+
+  ngOnInit(): void {
+    this.apiService.fetchPokemons();
+
+    this.activeSubscription.push(
+      this.apiService.fetching.subscribe((data) => {
+        this.fetching = data;
+      })
+    );
+
+    this.activeSubscription.push(
+      this.apiService.pokemons.subscribe((data) => {
+        this.pokemons = data;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.activeSubscription.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
 }
---
+++ src/app/services/poke-api.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { forkJoin, Observable, BehaviorSubject, switchMap } from 'rxjs';
+import { Pokemon } from '../types/Pokemon';
+import { PokemonList } from '../types/PokemonList';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeApiService {
+  apiUrl = 'https://pokeapi.co/api/v2/';
+
+  pokemons = new BehaviorSubject<Pokemon[]>([]);
+
+  fetching = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {}
+
+  private getPokemonList(): Observable<PokemonList> {
+    this.fetching.next(true);
+    return this.http.get(
+      this.apiUrl + 'pokemon?limit=20&offset=0/'
+    ) as Observable<PokemonList>;
+  }
+
+  private getPokemon(names: string[]): Observable<Pokemon[]> {
+    const requests: Observable<Pokemon>[] = [];
+    names.forEach((name) => {
+      requests.push(
+        this.http.get(this.apiUrl + 'pokemon/' + name) as Observable<Pokemon>
+      );
+    });
+
+    return forkJoin(requests);
+  }
+
+  fetchPokemons() {
+    this.getPokemonList()
+      .pipe(
+        switchMap((data) => {
+          const pokemonList: string[] = data.results.map((pokemon) => {
+            return pokemon.name;
+          });
+
+          return this.getPokemon(pokemonList);
+        })
+      )
+      .subscribe((data) => {
+        this.pokemons.next(data);
+        this.fetching.next(false);
+      });
+  }
+}
